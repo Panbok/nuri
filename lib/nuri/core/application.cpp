ca7d6d37dadf1fe779445cb370cee58674b55418
Yaroslav Panok <yapanek@gmail.com>
2026-02-04 11:03:39 +0200

    feat(application): refactor Application class to utilize GPUDevice and Window classes, enhance logging, and improve event handling

--- lib/nuri/core/application.cpp
+++ lib/nuri/core/application.cpp
@@ -1,49 +1,77 @@
 #include "nuri/core/application.h"
+#include "nuri/core/log.h"
+#include "nuri/core/window.h"
+#include "nuri/gfx/gpu_device.h"
+#include "nuri/gfx/renderer.h"
 
 namespace nuri {
 
 Application::Application(const std::string &title, std::int32_t width,
                          std::int32_t height)
-    : title_(title), width_(width), height_(height), window_(nullptr) {
-  minilog::initialize(nullptr, {.threadNames = false});
-  window_ = lvk::initWindow(title_.c_str(), width_, height_, true, false);
-  context_ = lvk::createVulkanContextWithSwapchain(
-      window_, static_cast<std::uint32_t>(width_),
-      static_cast<std::uint32_t>(height_), lvk::ContextConfig{});
-  renderer_ = nuri::Renderer::create(*context_);
-
-  glfwSetWindowUserPointer(window_, this);
-
-  glfwSetWindowSizeCallback(
-      window_, [](GLFWwindow *window, int width, int height) {
-        Application *app =
-            static_cast<Application *>(glfwGetWindowUserPointer(window));
-        app->getRenderer()->onResize(width, height);
-      });
+    : title_(title), width_(width), height_(height) {
+  Log::initialize({
+      .filePath =
+          std::filesystem::path(
+              std::format(
+                  "logs/{}_nuri.log",
+                  std::chrono::system_clock::now().time_since_epoch().count()))
+              .string(),
+      .logLevel = LogLevel::Debug,
+      .consoleLevel = LogLevel::Debug,
+      .threadNames = false,
+  });
+  window_ = Window::create(title_, width_, height_);
+  gpu_ = GPUDevice::create(*window_);
+  renderer_ = Renderer::create(*gpu_);
 }
 
 Application::~Application() {
-  context_.reset();
-  glfwDestroyWindow(window_);
-  glfwTerminate();
-  minilog::deinitialize();
+  renderer_.reset();
+  gpu_.reset();
+  window_.reset();
+  Log::shutdown();
 }
 
 void Application::run() {
   onInit();
 
-  while (!glfwWindowShouldClose(window_)) {
+  while (!gpu_->shouldClose()) {
     onUpdate(0.0f);
-    glfwPollEvents();
-    glfwGetFramebufferSize(window_, &width_, &height_);
-    if (!width_ || !height_)
+    gpu_->pollEvents();
+    std::int32_t newWidth = 0;
+    std::int32_t newHeight = 0;
+    gpu_->getFramebufferSize(newWidth, newHeight);
+    if (!newWidth || !newHeight) {
+      width_ = newWidth;
+      height_ = newHeight;
       continue;
+    }
+
+    if (newWidth != width_ || newHeight != height_) {
+      width_ = newWidth;
+      height_ = newHeight;
+      onResize(width_, height_);
+      renderer_->onResize(width_, height_);
+    }
+
     onDraw();
   }
 
   onShutdown();
 }
 
-double Application::getTime() const { return glfwGetTime(); }
+double Application::getTime() const { return gpu_->getTime(); }
+
+GPUDevice &Application::getGPU() { return *gpu_; }
+
+const GPUDevice &Application::getGPU() const { return *gpu_; }
+
+Window &Application::getWindow() { return *window_; }
+
+const Window &Application::getWindow() const { return *window_; }
+
+Renderer &Application::getRenderer() { return *renderer_; }
+
+const Renderer &Application::getRenderer() const { return *renderer_; }
 
 } // namespace nuri
--- lib/nuri/core/application.h
+++ lib/nuri/core/application.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "nuri/defines.h"
-#include "nuri/gfx/renderer.h"
 #include "nuri/pch.h"
 
 namespace nuri {
+
+class GPUDevice;
+class Renderer;
+class Window;
+
 class NURI_API Application {
 public:
   Application(const std::string &title, std::int32_t width,
@@ -22,23 +26,31 @@ public:
   virtual void onInit() = 0;
   virtual void onUpdate(float deltaTime) = 0;
   virtual void onDraw() = 0;
+  virtual void onResize(std::int32_t width, std::int32_t height) = 0;
   virtual void onShutdown() = 0;
 
-  inline std::unique_ptr<lvk::IContext> &getContext() { return context_; }
+  GPUDevice &getGPU();
+  const GPUDevice &getGPU() const;
+  Window &getWindow();
+  const Window &getWindow() const;
+
   inline float getAspectRatio() const {
     return width_ / static_cast<float>(height_);
   }
   inline std::int32_t getWidth() const { return width_; }
   inline std::int32_t getHeight() const { return height_; }
-  inline std::unique_ptr<nuri::Renderer> &getRenderer() { return renderer_; }
+
+  Renderer &getRenderer();
+  const Renderer &getRenderer() const;
 
 private:
   std::string title_;
   std::int32_t width_;
   std::int32_t height_;
-  lvk::LVKwindow *window_;
-  std::unique_ptr<lvk::IContext> context_;
-  std::unique_ptr<nuri::Renderer> renderer_;
+  std::unique_ptr<Window> window_;
+  std::unique_ptr<GPUDevice> gpu_;
+  std::unique_ptr<Renderer> renderer_;
 };
 
 } // namespace nuri
+
